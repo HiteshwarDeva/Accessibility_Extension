052e2d8145123103386385bf6a5a84e6349c5370
Baduka Naga Bhushan <nagabhushanbadka@gmail.com>
2025-12-01 11:16:27 +0530

    Updated UI as Per Details Figma

--- legacy_src/devtools/devtools.js
+++
@@ -1,5 +0,0 @@
-// Create a DevTools panel that loads the UI from panel/panel.html
-console.log('Hello!!')
-chrome.devtools.panels.create("Accessibility Minimal", "", "panel/panel.html", function(panel) {
-  console.log("Accessibility Minimal panel created");
-});
